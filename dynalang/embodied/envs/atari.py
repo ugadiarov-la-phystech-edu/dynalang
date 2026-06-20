db705c3fe99b0c4b5cb2b4999bc0d53e6db5e67b
AntoninaKOR <t0nykar@yandex.ru>
2026-06-20 11:26:18 +0300

    fix atari

--- dynalang/embodied/envs/atari.py
+++ dynalang/embodied/envs/atari.py
@@ -1,68 +1,91 @@
+import collections
+import os
+import threading
+
+import ale_py
+import ale_py.roms as roms
 import embodied
 import numpy as np
 
 
 class Atari(embodied.Env):
 
-  LOCK = None
+  LOCK = threading.Lock()
+  WEIGHTS = np.array([0.299, 0.587, 1 - (0.299 + 0.587)])
+  ACTION_MEANING = (
+      'NOOP', 'FIRE', 'UP', 'RIGHT', 'LEFT', 'DOWN', 'UPRIGHT', 'UPLEFT',
+      'DOWNRIGHT', 'DOWNLEFT', 'UPFIRE', 'RIGHTFIRE', 'LEFTFIRE', 'DOWNFIRE',
+      'UPRIGHTFIRE', 'UPLEFTFIRE', 'DOWNRIGHTFIRE', 'DOWNLEFTFIRE')
 
   def __init__(
-      self, name, repeat=4, size=(84, 84), gray=True, noops=0, lives=False,
-      sticky=True, actions='all', length=108000, seed=None, **unused):
+      self, name, repeat=4, size=(84, 84), gray=True, noops=0, lives='unused',
+      sticky=True, actions='all', length=108000, pooling=2, aggregate='max',
+      resize='pillow', autostart=False, clip_reward=False, seed=None, **unused):
     del unused
-    if lives in ('unused', 'none', None):
-      lives = False
-    else:
-      lives = bool(lives)
+    lives = self._normalize_lives(lives)
+    assert lives in ('unused', 'discount', 'reset'), lives
+    assert actions in ('all', 'needed'), actions
+    assert resize in ('opencv', 'pillow'), resize
+    assert aggregate in ('max', 'mean'), aggregate
+    assert pooling >= 1, pooling
+    assert repeat >= 1, repeat
     assert size[0] == size[1]
-
-    if self.LOCK is None:
-      import multiprocessing as mp
-      mp = mp.get_context('spawn')
-      self.LOCK = mp.Lock()
-
-    import cv2
-    self._cv2 = cv2
-
-    # if 'ATARI_ROMS' in os.environ:  # TODO
-    #   import absl.flags
-    #   import atari_py  # noqa: For flag definition.
-    #   absl.flags.FLAGS.atari_roms_path = os.environ['ATARI_ROMS']
-
-    # from PIL import Image
-    # self._image = Image
-
-    import gym.envs.atari
     if name == 'james_bond':
       name = 'jamesbond'
-    self._repeat = repeat
-    self._size = size
-    self._gray = gray
-    self._noops = noops
-    self._lives = lives
-    self._sticky = sticky
-    self._length = length
-    self._random = np.random.RandomState(seed)
+
+    self.repeat = repeat
+    self.size = size
+    self.gray = gray
+    self.noops = noops
+    self.lives = lives
+    self.sticky = sticky
+    self.length = length
+    self.pooling = pooling
+    self.aggregate = aggregate
+    self.resize = resize
+    self.autostart = autostart
+    self.clip_reward = clip_reward
+    self.rng = np.random.default_rng(seed)
+
     with self.LOCK:
-      self._env = gym.envs.atari.AtariEnv(
-          game=name,
-          obs_type='image',  # TODO: 'rgb' in new alepy.
-          frameskip=1, repeat_action_probability=0.25 if sticky else 0.0,
-          full_action_space=(actions == 'all'))
-    assert self._env.unwrapped.get_action_meanings()[0] == 'NOOP'
-    shape = self._env.observation_space.shape
-    self._buffer = [np.zeros(shape, np.uint8) for _ in range(2)]
-    self._ale = self._env.unwrapped.ale
-    self._last_lives = None
-    self._done = True
-    self._step = 0
+      self.ale = ale_py.ALEInterface()
+      self.ale.setLoggerMode(ale_py.LoggerMode.Error)
+      self.ale.setInt(b'random_seed', int(self.rng.integers(0, 2 ** 31)))
+      path = os.environ.get('ALE_ROM_PATH', None)
+      if path:
+        self.ale.loadROM(os.path.join(path, f'{name}.bin'))
+      else:
+        self.ale.loadROM(roms.get_rom_path(name))
+
+      self.ale.setFloat(
+          'repeat_action_probability', 0.25 if sticky else 0.0)
+      self.actionset = {
+          'all': self.ale.getLegalActionSet,
+          'needed': self.ale.getMinimalActionSet,
+      }[actions]()
+
+    W, H = self.ale.getScreenDims()
+    self.buffers = collections.deque(
+        [np.zeros((W, H, 3), np.uint8) for _ in range(self.pooling)],
+        maxlen=self.pooling)
+    self.prevlives = None
+    self.duration = None
+    self.done = True
+
+  @staticmethod
+  def _normalize_lives(lives):
+    if lives in ('unused', 'discount', 'reset'):
+      return lives
+    if lives in (False, None, 'none', 'False', 'false'):
+      return 'unused'
+    if lives in (True, 'True', 'true'):
+      return 'discount'
+    return lives
 
   @property
   def obs_space(self):
-    shape = self._size + (1 if self._gray else 3,)
     return {
-        'image': embodied.Space(np.uint8, shape),
-        # 'ram': embodied.Space(np.uint8, 128),
+        'image': embodied.Space(np.uint8, (*self.size, 1 if self.gray else 3)),
         'reward': embodied.Space(np.float32),
         'is_first': embodied.Space(bool),
         'is_last': embodied.Space(bool),
@@ -72,75 +95,91 @@ class Atari(embodied.Env):
   @property
   def act_space(self):
     return {
-        'action': embodied.Space(np.int32, (), 0, self._env.action_space.n),
+        'action': embodied.Space(np.int32, (), 0, len(self.actionset)),
         'reset': embodied.Space(bool),
     }
 
   def step(self, action):
-    if action['reset'] or self._done:
-      with self.LOCK:
-        self._reset()
-      self._done = False
-      self._step = 0
+    if action['reset'] or self.done:
+      self._reset()
+      self.prevlives = self.ale.lives()
+      self.duration = 0
+      self.done = False
       return self._obs(0.0, is_first=True)
-    total = 0.0
-    dead = False
-    for repeat in range(self._repeat):
-      _, reward, over, info = self._env.step(action['action'])
-      self._step += 1
-      total += reward
-      if repeat == self._repeat - 2:
-        self._screen(self._buffer[1])
-      if over:
+    reward = 0.0
+    terminal = False
+    last = False
+    assert 0 <= action['action'] < len(self.actionset), action['action']
+    act = self.actionset[action['action']]
+    for repeat in range(self.repeat):
+      reward += self.ale.act(act)
+      self.duration += 1
+      if repeat >= self.repeat - self.pooling:
+        self._render()
+      if self.ale.game_over():
+        terminal = True
+        last = True
+      if self.duration >= self.length:
+        last = True
+      lives = self.ale.lives()
+      if self.lives == 'discount' and 0 < lives < self.prevlives:
+        terminal = True
+      if self.lives == 'reset' and 0 < lives < self.prevlives:
+        terminal = True
+        last = True
+      self.prevlives = lives
+      if terminal or last:
         break
-      if self._lives:
-        current = self._ale.lives()
-        if current < self._last_lives:
-          dead = True
-          self._last_lives = current
-          break
-    if not self._repeat:
-      self._buffer[1][:] = self._buffer[0][:]
-    self._screen(self._buffer[0])
-    self._done = over or (self._length and self._step >= self._length)
-    return self._obs(total, is_last=self._done, is_terminal=dead or over)
+    self.done = last
+    return self._obs(reward, is_last=last, is_terminal=terminal)
 
   def _reset(self):
-    self._env.reset()
-    if self._noops:
-      for _ in range(self._random.randint(self._noops)):
-         _, _, dead, _ = self._env.step(0)
-         if dead:
-           self._env.reset()
-    self._last_lives = self._ale.lives()
-    self._screen(self._buffer[0])
-    self._buffer[1].fill(0)
+    with self.LOCK:
+      self.ale.reset_game()
+      for _ in range(self.rng.integers(self.noops + 1)):
+        self.ale.act(self.ACTION_MEANING.index('NOOP'))
+        if self.ale.game_over():
+          self.ale.reset_game()
+      if self.autostart and self.ACTION_MEANING.index('FIRE') in self.actionset:
+        self.ale.act(self.ACTION_MEANING.index('FIRE'))
+        if self.ale.game_over():
+          self.ale.reset_game()
+        self.ale.act(self.ACTION_MEANING.index('UP'))
+        if self.ale.game_over():
+          self.ale.reset_game()
+    self._render()
+    for i, dst in enumerate(self.buffers):
+      if i > 0:
+        np.copyto(dst, self.buffers[0])
+
+  def _render(self):
+    self.buffers.appendleft(self.buffers.pop())
+    self.ale.getScreenRGB(self.buffers[0])
 
   def _obs(self, reward, is_first=False, is_last=False, is_terminal=False):
-    np.maximum(self._buffer[0], self._buffer[1], out=self._buffer[0])
-    image = self._buffer[0]
-    if self._gray:
-      weights = [0.299, 0.587, 1 - (0.299 + 0.587)]
-      image = np.tensordot(image, weights, (-1, 0)).astype(image.dtype)
-    if image.shape[:2] != self._size:
-      # image = self._image.fromarray(image)
-      # image = image.resize(self._size, self._image.NEAREST)
-      # image = np.array(image)
-      image = self._cv2.resize(
-          image, self._size, interpolation=self._cv2.INTER_AREA)
-    if self._gray:
-      image = image[:, :, None]
+    if self.clip_reward:
+      reward = np.sign(reward)
+    if self.aggregate == 'max':
+      image = np.amax(self.buffers, 0)
+    elif self.aggregate == 'mean':
+      image = np.mean(self.buffers, 0).astype(np.uint8)
+    if self.resize == 'opencv':
+      import cv2
+      image = cv2.resize(image, self.size, interpolation=cv2.INTER_AREA)
+    elif self.resize == 'pillow':
+      from PIL import Image
+      image = Image.fromarray(image)
+      image = image.resize(self.size, Image.BILINEAR)
+      image = np.array(image)
+    if self.gray:
+      image = (image * self.WEIGHTS).sum(-1).astype(image.dtype)[:, :, None]
     return dict(
         image=image,
-        # ram=self._env.env._get_ram(),
-        reward=reward,
+        reward=np.float32(reward),
         is_first=is_first,
         is_last=is_last,
         is_terminal=is_last,
     )
 
-  def _screen(self, array):
-    self._ale.getScreenRGB2(array)
-
   def close(self):
-    return self._env.close()
+    return None
